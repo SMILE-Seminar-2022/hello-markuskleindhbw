a115182b8e0444abc32342e47269ec2b84649f07
markuskleindhbw <101702301+markuskleindhbw@users.noreply.github.com>
2023-05-10 11:55:57 +0200

    Update MessageUtil.java

--- MessageUtil.java
+++ MessageUtil.java
@@ -1,22 +1,18 @@
-package GitHub_Test;
-
 public class MessageUtil {
-
-	   private String message;
-
-	   //Constructor
-	   //@param message to be printed
-	   public MessageUtil(String message){
-	      this.message = message;
-	   }
-
-	   // prints the message
-	   public String printMessage(){
-	      System.out.println("Hello World");
-	      return message;
-	   }
-		// returns the length of the message
-	    public int getMessageLength() {
-	       return message.length();
-	}
+	
+private String message;
+//Constructor
+//@param message to be printed
+public MessageUtil(String message){
+this.message = message;
+}
+// prints the message
+public String printMessage(){
+//System.out.println(message);
+return message;
+}
+// returns the length of the message
+//public int getMessageLength() {
+//return message.length();
+// }
 }
