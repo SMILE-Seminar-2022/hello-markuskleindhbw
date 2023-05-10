b1061d10d4279686afa03fc22380fd8588ec931b
markuskleindhbw <101702301+markuskleindhbw@users.noreply.github.com>
2023-05-10 10:54:45 +0200

    Update feedback.java

--- feedback.java
+++ feedback.java
@@ -7,7 +7,10 @@ public class feedback {
 
    String message = "Hello World";	
    MessageUtil messageUtil = new MessageUtil(message);
-
+@Test public void testMessageLength() { 
+  int expectedLength = message.length(); 
+  int actualLength = messageUtil.getMessageLength(); 
+   assertEquals(expectedLength, actualLength); }
    @Test
    public void testPrintMessage() {
       assertEquals(message,messageUtil.printMessage());
