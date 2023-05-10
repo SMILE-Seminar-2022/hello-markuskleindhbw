b1eb4bafc9423b5756d6c9b5b92ee57ded561bc6
markuskleindhbw <101702301+markuskleindhbw@users.noreply.github.com>
2023-05-10 11:21:01 +0200

    Update and rename feedback.java to feedbackTest.java

---
+++ feedbackTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+ 
+public class feedbackTest {
+
+   String message = "Hello World";    
+   MessageUtil messageUtil = new MessageUtil(message);
+ 
+   @Test
+   public void testPrintMessage() {
+      assertEquals(message,messageUtil.printMessage());
+   }
+
+   @Test
+   public void testMessageLength() {
+       int expectedLength = message.length();
+       int actualLength = messageUtil.getMessageLength();
+       assertEquals(expectedLength, actualLength);
+   }
+}
