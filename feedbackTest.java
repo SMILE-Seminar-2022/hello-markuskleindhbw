29a3ebcb302aa692c8129975bd282cbfd526ede7
markuskleindhbw <101702301+markuskleindhbw@users.noreply.github.com>
2023-05-10 11:57:06 +0200

    Update feedbackTest.java

--- feedbackTest.java
+++ feedbackTest.java
@@ -1,20 +1,25 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
- 
+
 public class feedbackTest {
 
    String message = "Hello World";    
    MessageUtil messageUtil = new MessageUtil(message);
- 
+  
    @Test
    public void testPrintMessage() {
       assertEquals(message,messageUtil.printMessage());
    }
-
+    /*
+     * @Test public void testMessageLength() {int expectedLength =
+     * message.length(); int actualLength = messageUtil.getMessageLength();
+     * assertEquals(expectedLength, actualLength); }
+     */
    @Test
-   public void testMessageLength() {
-       int expectedLength = message.length();
-       int actualLength = messageUtil.getMessageLength();
-       assertEquals(expectedLength, actualLength);
+   public void testPrintMessageWithDifferentMessage() {
+      String differentMessage = "Goodbye";
+      MessageUtil differentMessageUtil = new MessageUtil(differentMessage);
+      assertEquals(differentMessage, differentMessageUtil.printMessage());
    }
 }
+
